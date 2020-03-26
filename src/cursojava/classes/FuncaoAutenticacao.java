867127fa9b17f3317b20d1be1144ead53f0f82d1
jonson <luizlindonjonson@yahoo.com.br>
2020-03-26 00:31:53 -0300

    update

---
+++ src/cursojava/classes/FuncaoAutenticacao.java
@@ -0,0 +1,14 @@
+package cursojava.classes;
+
+public class FuncaoAutenticacao {
+
+	public FuncaoAutenticacao(Diretor diretor) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean autenticar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
